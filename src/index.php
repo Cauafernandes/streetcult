c772aecafea15270849a6169f1268789b2e98c9f
Cauã Fernandes <caua.rules@hotmail.com>
2018-02-28 10:42:08 -0300

    Project 1.4

--- src/index.php
+++ src/index.php
@@ -13,8 +13,26 @@
       <div class="container">
          <div class="row">
             <div class="col-12">
+                <ul class="frms-pR">
+                    <li>
+                        <i class="fas fa-truck"></i>
+                        <h4>FRETE GRÁTIS</h4>
+                        <p>FRETE GRÁTIS SUDESTE 99,00 - OUTROS ESTADOS A PARTIR DE 149,00</p>
+                    </li>
+                    <li>
+                        <i class="fas fa-credit-card"></i>
+                        <h4>NO CARTÃO OU BOLETO</h4>
+                        <p>ATÉ 6 VEZES SEM JUROS COM TODOS OS CARTÕES DE CRÉDITO - DÉBITO</p>
+                    </li>
+                    <li>
+                        <i class="fas fa-percent"></i>
+                        <h4>DESCONTOS À VISTA</h4>
+                        <p>10% DE DESCONTO NAS COMPRAS POR BOLETO OU TRANSFERÊNCIA BANCÁRIA</p>
+                    </li>
+                </ul>
                 <div class="mn-pR">
                     <div class="catpr">
+                        <i class="fas fa-chess-king"></i>
                         <h2>LANÇAMENTOS</h2>
                     </div>
                     <ul class="lancamentos">
